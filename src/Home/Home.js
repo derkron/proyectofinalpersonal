9ccc7279fa2e8a8845f78d754feec12914ed2ae5
407 <407@academicos.cesde.edu.co>
2022-11-29 16:38:21 -0500

    se inicia con react

---
+++ src/Home/Home.js
@@ -0,0 +1,98 @@
+import './Home.css'
+export function Home(){
+
+    return(
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+            <section>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <h2>SKILLET</h2>
+                            <p>
+                            Skillet es una banda de rock cristiano y metal cristiano​​​ originaria de Tennessee, fundada en 1996 y conformada por John y Korey Cooper, Seth Morrison y Jen Ledger. El grupo ha publicado once álbumes de estudio, el más reciente, Dominion, en el 2022.
+                            </p>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotycgm.appspot.com/o/bgt.jpg?alt=media&token=1b94f739-d7e5-4893-84c4-4ce5433f4c87"></img>
+
+                        </div>
+                        <div class="col-12 col-md-4 ">
+                        
+                            <div class="row">
+                                <div class="col-1">
+                                    <i class="bi bi-vinyl fs-1"></i>
+                                </div>
+                                <div class="col">
+                                    <div class="row">
+                                        <h1>Cali</h1>
+                                    </div>
+                                    <div class="row">
+                                        <h1>Dec 10</h1>
+                                    </div>
+                                </div>
+                            </div>
+
+                             <div class="row">
+                                <div class="col-1">
+                                    <i class="bi bi-vinyl fs-1"></i>
+                                </div>
+                                <div class="col">
+                                    <div class="row">
+                                        <h1>Bogota</h1>
+                                    </div>
+                                    <div class="row">
+                                        <h1>Dec 25</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-1">
+                                    <i class="bi bi-vinyl fs-1"></i>
+                                </div>
+                                <div class="col">
+                                    <div class="row">
+                                        <h1>Barranquilla</h1>
+                                    </div>
+                                    <div class="row">
+                                        <h1>Dec 30</h1>
+                                    </div>
+                                </div>
+                            </div>                            
+                            
+                        
+                        
+                        
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </section>
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotycgm.appspot.com/o/skillet2.jpg?alt=media&token=43df7d3c-def2-493d-890d-882d9bcf8c4c" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotycgm.appspot.com/o/skillet%203.jpeg?alt=media&token=8981d434-91bd-4a20-8277-be6d338c6ed1" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotycgm.appspot.com/o/1575494120.webp?alt=media&token=666fe090-c8a0-495c-b9cd-a0b660ed0379" alt="" class="img-fluid w-100"/>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+
+}
